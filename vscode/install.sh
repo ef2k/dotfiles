1120fc029d13294d374bd6172d9b57b33835931b
Eddie Flores <ef2k@users.noreply.github.com>
2020-04-19 01:01:47 -0400

    Fix install script for vscode and hide activity bar

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-ln -s $HOME/dotfiles/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
-ln -s $HOME/dotfiles/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
-./extensions.sh
+ln -s $HOME/dotfiles/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json;
+ln -s $HOME/dotfiles/vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json;
+./extensions.sh;
